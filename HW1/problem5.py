e3756df20b93bdc948cc59f8a1f5b2251b6cb65c
RickyMatsumoto <43117303+RickyMatsumoto@users.noreply.github.com>
2021-09-05 19:31:01 -0500

    update

--- HW1/problem5.py
+++ HW1/problem5.py
@@ -3,3 +3,16 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import scipy.stats
 import math
+import os
+
+df = pd.read_csv(os.getcwd() + "\HW1\PatientData.csv", header=None)
+print("# Patients = " + str(len(df)))
+print("# Features = " + str(len(df.columns)))
+# print(df.head())
+print("The first four features seem to be age, sex(0m, 1f), height(cm), and weight(kg).")
+for column in df:
+    temp = df[column]
+    # print(temp)
+    temp.replace(to_replace='?', value=None)
+    mean = temp.mean()
+    temp.fillna(value=mean)
